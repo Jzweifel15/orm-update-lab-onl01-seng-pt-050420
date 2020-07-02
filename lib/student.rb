bdff5dbfe6f24134067ffbee37a0b34771c28b24
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-02 14:16:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,10 +40,10 @@ class Student
   end
   
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    student = self.new(id, name, grade)
+    @id = row[0]
+    @name = row[1]
+    @grade = row[2]
+    student = self.new(@id, @name, @grade)
     student
   end
   
